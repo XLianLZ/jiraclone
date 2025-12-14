0bba6b1193551196c5745c64b5cd6bf5dd11b3d6
XLianLZ <lianbox7304@gmail.com>
2025-12-14 00:44:25 -0500

    finish issue class

--- src/main/java/com/code/jiraclone/model/entities/Issue.java
+++ src/main/java/com/code/jiraclone/model/entities/Issue.java
@@ -1,10 +1,18 @@
 package com.code.jiraclone.model.entities;
 
+import com.code.jiraclone.model.enums.Priority;
+import com.code.jiraclone.model.enums.Status;
+import com.code.jiraclone.model.enums.Type;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -18,9 +26,54 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 5000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "epic_id")
+    private Epic epic;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_user_id")
     private User assignedUser;
 
-    public void setAssignedUser(User user){
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
 
-    }
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime startingDate;
+
+    @Column
+    private LocalDateTime estimatedDate;
 }
