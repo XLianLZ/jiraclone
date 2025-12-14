f601f174661357fa76ca740b0425ea45155675f2
XLianLZ <lianbox7304@gmail.com>
2025-12-14 00:44:18 -0500

    finish epic class

--- src/main/java/com/code/jiraclone/model/entities/Epic.java
+++ src/main/java/com/code/jiraclone/model/entities/Epic.java
@@ -1,15 +1,53 @@
 package com.code.jiraclone.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.code.jiraclone.model.enums.Status;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "epics")
+@EntityListeners(AuditingEntityListener.class)
 public class Epic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @OneToMany(mappedBy = "epic", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Issue>issuesList = new HashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
