fd1c5924f56047171b29c67bd2133a9d0b71b446
XLianLZ <lianbox7304@gmail.com>
2025-12-14 00:39:33 -0500

    finish project entity

--- src/main/java/com/code/jiraclone/model/entities/Project.java
+++ src/main/java/com/code/jiraclone/model/entities/Project.java
@@ -1,10 +1,13 @@
 package com.code.jiraclone.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -15,6 +18,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Entity
 @Table(name = "projects")
+@EntityListeners(AuditingEntityListener.class)
 public class Project {
 
     @Id
@@ -30,17 +34,32 @@ public class Project {
     @Column(length = 1000)
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User projectOwner;
+
     @ManyToMany
-    \@JoinTable(
-            name = "project_members",
+    @JoinTable(
+            name="project_members",
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id")
     )
-    private Set<User> members = new HashSet<>();
+    @JsonIgnore
+    private Set<User> projectMembers = new HashSet<>();
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
-    private Set<Issue> issues = new HashSet<>();
+    @JsonIgnore
+    private Set<Issue> issuesList = new HashSet<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Epic> epicsList = new HashSet<>();
 
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
